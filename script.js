b08c2e392aa1b6a39a3b69acfa63bbcf81c6b77a
SiddhiSingh25 <siddhisingh18102005@gmail.com>
2024-08-01 23:15:15 +0530

    update

--- script.js
+++ script.js
@@ -48,3 +48,47 @@ document.querySelector(".page4").addEventListener("mouseenter", ()=>{
         delay:1.6
     })
 })
+document.querySelector(".menu").addEventListener("click", ()=>{
+    document.querySelector("nav").style.paddingRight = 0;
+    document.querySelector(".navRight").style.alignItems = "center";
+    document.querySelector(".navRight").style.justifyContent = "flex-end";
+    document.querySelector(".navRight").style.flexDirection = "column";
+    document.querySelector(".navRight").style.width = "50%";
+    document.querySelector(".navRight").style.paddingBottom = "3vh";
+    document.querySelector(".navRight").style.gap = "4vh";
+    document.querySelectorAll(".navRight h1, .cart").forEach((e)=>{
+        e.style.display = "flex";
+    })
+    document.querySelector(".navRight").style.borderBottomLeftRadius = "44px";
+    document.querySelector(".menu").style.display = "none";
+    document.querySelector(".navRight").style.background = "#fff";
+    document.querySelector(".mark").style.display = "flex";
+    gsap.to(".navRight",{
+        height : "90vh",
+        duration :0.5
+    })
+})
+document.querySelector(".mark").addEventListener("click", ()=>{
+    document.querySelector("nav").style.paddingRight = "1.6vh";
+    document.querySelector(".navRight").style.alignItems = "flex-end";
+    document.querySelector(".navRight").style.justifyContent = "center";
+    document.querySelector(".navRight").style.flexDirection = "none";
+    document.querySelector(".navRight").style.width = "45%";
+    document.querySelector(".navRight").style.paddingBottom = "0";
+    document.querySelector(".navRight").style.gap = "0";
+    document.querySelectorAll(".navRight h1, .cart").forEach((e)=>{
+        e.style.display = "none";
+    })
+    document.querySelector(".navRight").style.borderBottomLeftRadius = "0";
+    document.querySelector(".navRight").style.background = "#fff";
+    document.querySelector(".mark").style.display = "none";
+    gsap.to(".navRight",{
+        height : "8.5vh",
+        duration :0.5
+    })
+    gsap.to(".menu",{
+        display: "flex",
+        duration :0.5,
+        delay :0.2
+    })
+})
